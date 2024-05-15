9b93a5c6a2bbd85ff57967b1c85b7400c65a85ba
beka-zhvania <beka.zhvania@rwth-aachen.de>
2024-05-15 10:09:15 +0200

    Add encryption and decryption functionalities

---
+++ EncryptionTool/EncryptionHelper.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EncryptionTool
+
+{
+    public static class EncryptionHelper
+    {
+        // Method to encrypt plain text using AES encryption
+        public static byte[] Encrypt(string plainText, byte[] key, byte[] iv)
+        {
+            // Create an instance of the AES algorithm
+            using (Aes aesAlg = Aes.Create())
+            {
+                // Set the key and initialization vector (IV)
+                aesAlg.Key = key;
+                aesAlg.IV = iv;
+
+                // Create an encryptor object
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create a memory stream to hold the encrypted data
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    // Create a CryptoStream to perform encryption
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        // Write the plain text to the stream, which gets encrypted
+                        swEncrypt.Write(plainText);
+                    }
+
+                    // Return the encrypted data as a byte array
+                    return msEncrypt.ToArray();
+                }
+            }
+        }
+
+        // Method to decrypt cipher text using AES decryption
+        public static string Decrypt(byte[] cipherText, byte[] key, byte[] iv)
+        {
+            // Create an instance of the AES algorithm
+            using (Aes aesAlg = Aes.Create())
+            {
+                // Set the key and initialization vector (IV)
+                aesAlg.Key = key;
+                aesAlg.IV = iv;
+
+                // Create a decryptor object
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create a memory stream with the cipher text
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    // Create a CryptoStream to perform decryption
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+                        // Read the decrypted data from the stream and return as a string
+                        return srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+        }
+
+        // Method to generate a random key and IV for AES encryption
+        public static void GenerateKeyAndIV(out byte[] key, out byte[] iv)
+        {
+            // Create an instance of the AES algorithm
+            using (Aes aesAlg = Aes.Create())
+            {
+                // Assign the generated key and IV to the out parameters
+                key = aesAlg.Key;
+                iv = aesAlg.IV;
+            }
+        }
+
+        // Method to encrypt a file using AES encryption
+        public static void EncryptFile(string inputFile, string outputFile, byte[] key, byte[] iv)
+        {
+            // Open the input and output files
+            using (FileStream fsInput = new FileStream(inputFile, FileMode.Open, FileAccess.Read))
+            using (FileStream fsOutput = new FileStream(outputFile, FileMode.Create, FileAccess.Write))
+            using (Aes aesAlg = Aes.Create())
+            {
+                // Set the key and initialization vector (IV)
+                aesAlg.Key = key;
+                aesAlg.IV = iv;
+
+                // Create an encryptor object
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create a CryptoStream to perform encryption
+                using (CryptoStream csEncrypt = new CryptoStream(fsOutput, encryptor, CryptoStreamMode.Write))
+                {
+                    // Copy the contents of the input file to the CryptoStream, encrypting it
+                    fsInput.CopyTo(csEncrypt);
+                }
+            }
+        }
+
+        // Method to decrypt a file using AES decryption
+        public static void DecryptFile(string inputFile, string outputFile, byte[] key, byte[] iv)
+        {
+            // Open the input and output files
+            using (FileStream fsInput = new FileStream(inputFile, FileMode.Open, FileAccess.Read))
+            using (FileStream fsOutput = new FileStream(outputFile, FileMode.Create, FileAccess.Write))
+            using (Aes aesAlg = Aes.Create())
+            {
+                // Set the key and initialization vector (IV)
+                aesAlg.Key = key;
+                aesAlg.IV = iv;
+
+                // Create a decryptor object
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create a CryptoStream to perform decryption
+                using (CryptoStream csDecrypt = new CryptoStream(fsInput, decryptor, CryptoStreamMode.Read))
+                {
+                    // Copy the contents of the CryptoStream to the output file, decrypting it
+                    csDecrypt.CopyTo(fsOutput);
+                }
+            }
+        }
+    }
+
+}
--- EncryptionTool/Program.cs
+++ EncryptionTool/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace EncryptionTool
 {
@@ -10,6 +9,34 @@ namespace EncryptionTool
     {
         static void Main(string[] args)
         {
+            EncryptionHelper.GenerateKeyAndIV(out byte[] key, out byte[] iv);
+
+            //Console.WriteLine("Enter text to encrypt:");
+            //string plainText = Console.ReadLine();
+
+            //byte[] encrypted = EncryptionHelper.Encrypt(plainText, key, iv);
+            //Console.WriteLine($"Encrypted text: {Convert.ToBase64String(encrypted)}");
+
+            //string decrypted = EncryptionHelper.Decrypt(encrypted, key, iv);
+            //Console.WriteLine($"Decrypted text: {decrypted}");
+
+            // File encryption/decryption example with paths relative to project root directory
+            Console.WriteLine("Enter the relative path of the file to encrypt (relative to project root):");
+            string relativeInputFile = Console.ReadLine();
+
+            // Navigate up to project root directory (two levels up from bin\Debug\net5.0 or similar)
+            string projectRoot = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.FullName;
+            string inputFile = Path.Combine(projectRoot, relativeInputFile);
+
+            string encryptedFile = Path.Combine(projectRoot, relativeInputFile + ".enc");
+            EncryptionHelper.EncryptFile(inputFile, encryptedFile, key, iv);
+            Console.WriteLine($"File encrypted to: {encryptedFile}");
+
+            string decryptedFile = Path.Combine(projectRoot, relativeInputFile + ".dec");
+            EncryptionHelper.DecryptFile(encryptedFile, decryptedFile, key, iv);
+            Console.WriteLine($"File decrypted to: {decryptedFile}");
+
+            Console.ReadLine();
         }
     }
 }
